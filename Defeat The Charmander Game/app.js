c53e5591a90da1b8f2ad3b1074bca46633c386f1
RafaelAndrade0 <rafael.asilva@dcomp.ufs.br>
2017-09-08 12:38:06 -0300

    Melhorando o Joguim

--- Defeat The Charmander Game/app.js
+++ Defeat The Charmander Game/app.js
@@ -1,15 +1,22 @@
-new vue({
+new Vue({
     el: "#vue-app",
     data: {
-        hp: 100
+        hp: 100,
+        endGame: false
     },
 
     methods: {
         punch: function() {
-            this.hp -= 10
+            this.hp -= 10;
+            console.log(this.hp);
+            if (this.hp <= 0) {
+                this.endGame = true;
+            }
         },
         restart: function() {
-            this.hp = 100
+            this.hp = 100;
+            console.log(this.hp);
+            this.endGame = false;
         }
     },
 
